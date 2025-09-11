cb2dcdf75e9614b18b31a2163a47ebbadc144e53
Jenn <znzhenn@gmail.com>
2025-09-10 22:46:31 -0400

    checkpoint1pt5
    * part 5 (going insane slowly </3)

--- src/checkpoint1pt5.java
+++ src/checkpoint1pt5.java
@@ -3,19 +3,7 @@ public class checkpoint1pt5 {
     public static void main(String[] args) {
     
     	int value = 89;
-        System.out.println(check(value));
-        
         System.out.println("Hello world");
         System.out.println("Please fail no longer");
-    }
-
-    public boolean check(int value) {
-        if (value == 99) {
-            return true;
-        }
-        if (value == 0) {
-            return false;
-        }
-        return value == 1;
     }
 }
