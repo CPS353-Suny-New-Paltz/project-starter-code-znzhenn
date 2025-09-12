53e722fbce69be53f2513cb87b5ecf9858c3e75d
Jenn <znzhenn@gmail.com>
2025-09-12 12:05:10 -0400

    checkpoint1pt6
    * part 6 (forgot to get approval from reviewer)

---
+++ src/Checkpoint1pt6.java
@@ -0,0 +1,18 @@
+public class Checkpoint1pt6 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		
+		
+		int[] badArray = {1,2,3,4, 0, 1, -1}; 
+		//difficulty printing magic number
+		
+		for (int i=0; i<badArray.length-1; i++) {
+			System.out.println(i);
+			i++;
+			
+		} // waiting for review approval
+	}
+}
