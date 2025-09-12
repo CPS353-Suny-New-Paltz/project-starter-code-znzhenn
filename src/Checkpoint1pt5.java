5558cce307e0dc089382c99e25e18290f9cf57f5
Jenn <155208622+znzhenn@users.noreply.github.com>
2025-09-12 11:53:35 -0400

    Checkpoint 1 (part 5)
    * checkpoint1pt5
    * part 5 (bad code written forgetting the curly bracket, expected to fail)
    * Revert "checkpoint1pt5"
    * checkpoint1pt5
    * part 5 (intentionally forgetting right bracket)
    * checkpoint1pt5-revision1
    * part 5 (magic number present in the array, should be flagged as bad)
    * checkpoint1pt5-revision2
    * part 5 (file name changed correctly)
    * checkpoint1pt5-revision3
    * part 5 (removal of magic numbers)

---
+++ src/Checkpoint1pt5.java
@@ -0,0 +1,18 @@
+public class Checkpoint1pt5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		
+		
+		int[] badArray = {0, 1, -1}; 
+		//difficulty printing magic number
+		
+		for (int i=0; i<badArray.length-1; i++) {
+			System.out.println(i);
+			i++;
+			
+		} //replaced the bracket here
+	}
+}
