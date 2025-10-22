bc2495625176dcbb8113a5ee06e0483293b729cf
Jenn <znzhenn@gmail.com>
2025-10-22 02:11:10 -0400

    Checkpoint 3 (pt2) - update 2
    * data storage api implementation :')
    * trying to sort out misc things

---
+++ src/projectapis/process/DataStorageAPIImplementation.java
@@ -0,0 +1,36 @@
+package projectapis.process;
+
+import projectapis.ComputationStatus;
+
+public class DataStorageAPIImplementation implements DataStorageAPI {
+
+    @Override
+    public void saveData(String data) {
+        // Example implementation
+        System.out.println("Saving data: " + data);
+    }
+
+    @Override
+    public String loadData() {
+        // Example implementation
+        return "Loaded data";
+    }
+
+	@Override
+	public void saveComputation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long fetchComputation() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ComputationStatus getComputationStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
