f380d3fd68e767975e131f07ce47e09a9804fd5e
Jenn <155208622+znzhenn@users.noreply.github.com>
2025-10-23 02:42:10 -0400

    Checkpoint 3 edits (#18)
    * Checkpoint 3 Edits - update 1
    
    * creation of the branch and updating implementation
    
    * editing methods
    
    * implementing methods instead of having them be basic/defaults
    
    * spelling error corrected
    
    * checkstyle test edits
    
    * syntax :')
    
    * removed return type on constructor
    
    * api implementation updates
    
    * if loop edit

--- src/projectapis/conceptual/FactorialAPIImplementation.java
+++ src/projectapis/conceptual/FactorialAPIImplementation.java
@@ -11,27 +11,31 @@ public class FactorialAPIImplementation implements FactorialAPI {
 	
     @Override
     public int computeFactorial(int n) {
-    	int result = 1;
-    	for (int i = 2; i <= n; i++) {
-    		result *= i;
+    	if (n < 0) {
+    		throw new IllegalArgumentException("n must be >= 0");
     	}
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
         return result;
     }
    
     @Override
     public long computeDigitFactorialSum(int number) {
         long sum = 0;
-        while (number > 0) {
-            int digit = number % 10;
+        int temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
             sum += computeFactorial(digit);
-            number /= 10;
+            temp /= 10;
         }
         return sum;
     }
 
 	@Override
 	public long factorialOfSum() {
-		// TODO Auto-generated method stub
-		return 0;
+		int number = 247;
+		return computeDigitFactorialSum(number);
 	}
 }
