5fff189b1d6e629eabc55eaf16f435ac2d5fd410
Jenn <znzhenn@gmail.com>
2025-10-23 00:34:35 -0400

    editing methods
    * implementing methods instead of having them be basic/defaults

--- src/projectapis/conceptual/FactorialAPIImplementation.java
+++ src/projectapis/conceptual/FactorialAPIImplementation.java
@@ -26,4 +26,10 @@ public class FactorialAPIImplementation implements FactorialAPI {
         }
         return sum;
     }
+
+	@Override
+	public long factorialOfSum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
