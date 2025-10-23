d92ce99f1fdb2f6fdab7ac0c2d4da15584a32129
Jenn <znzhenn@gmail.com>
2025-10-23 02:51:12 -0400

    integration test edits

--- test/integrationtests/InMemoryDataStorageAPI.java
+++ test/integrationtests/InMemoryDataStorageAPI.java
@@ -1,72 +1,60 @@
 package integrationtests;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import projectapis.ComputationStatus;
 import projectapis.process.DataStorageAPI;
 
 public class InMemoryDataStorageAPI implements DataStorageAPI {
-	
-	private final InMemoryInput input;
-	private final InMemoryOutput output;
-	
-	public InMemoryDataStorageAPI(InMemoryInput input, InMemoryOutput output) {
+
+    private final InMemoryInput input;
+    private final InMemoryOutput output;
+    private boolean computationDone = false;
+    private List<Long> storedResults = new ArrayList<>();
+
+    public InMemoryDataStorageAPI(InMemoryInput input, InMemoryOutput output) {
         this.input = input;
         this.output = output;
     }
-	
-	public List<Integer> readInput() {
-		return input.getIntegers();
-	}
-	
-	public void storeDataResults(String key, List<Long> results){
-		for (Long result : results) {
-			output.write(String.valueOf(result));
-		}
-		
-	}
 
-	@Override
-	public List<Integer> loadIntegers(String inputSource, String delimiter) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<Integer> loadIntegers(String inputSource, String delimiter) {
+        return input.getInput();
+    }
 
-	@Override
-	public void storeResults(String outputSource, List<Long> results) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void storeResults(String outputSource, List<Long> results) {
+        storedResults.clear();
+        storedResults.addAll(results);
+        computationDone = true;
+        for (Long r : results) {
+            output.write(String.valueOf(r));
+        }
+    }
 
-	@Override
-	public long fetchComputation() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public long fetchComputation() {
+        return storedResults.isEmpty() ? 0 : storedResults.get(0);
+    }
 
-	@Override
-	public ComputationStatus getComputationStatus() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ComputationStatus getComputationStatus() {
+        return computationDone ? ComputationStatus.EXISTS : ComputationStatus.NOT_EXISTS;
+    }
 
-	@Override
-	public String loadData() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String loadData() {
+        return "Loaded data"; // simple placeholder
+    }
 
-	@Override
-	public void saveData(String data) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void saveData(String data) {
+        output.write(data);
+    }
 
-	@Override
-	public void saveComputation() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
+    @Override
+    public void saveComputation() {
+        computationDone = true;
+    }
 }
