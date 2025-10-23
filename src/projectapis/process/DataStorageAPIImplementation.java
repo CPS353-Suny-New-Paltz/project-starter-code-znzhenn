748a7d9dedd86f6b6ee96aaa1376913c9d88095e
Jenn <znzhenn@gmail.com>
2025-10-22 21:24:42 -0400

    Checkpoin 3 (pt3) - update 1
    * gradle updating
    * making empty files for each of the test

--- src/projectapis/process/DataStorageAPIImplementation.java
+++ src/projectapis/process/DataStorageAPIImplementation.java
@@ -6,5 +6,39 @@ import project.annotations.ProcessAPI;
 
 @ProcessAPI
 public class DataStorageAPIImplementation implements DataStorageAPI {
-	//should be empty so that it fails
+
+    @Override
+    public void saveData(String data) {
+        System.out.println("Saving data: " + data);
+    }
+
+    @Override
+    public String loadData() {
+        return "Loaded data";
+    }
+
+    @Override
+    public void saveComputation() {
+        // Stub implementation
+    }
+
+    @Override
+    public long fetchComputation() {
+        return 0;
+    }
+
+    @Override
+    public ComputationStatus getComputationStatus() {
+        return null;
+    }
+
+    @Override
+    public List<Integer> loadIntegers(String inputSource, String delimiter) {
+        return null;
+    }
+
+    @Override
+    public void storeResults(String outputSource, List<Long> results) {
+        // Stub implementation
+    }
 }
