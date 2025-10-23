66cd85b43008a7439d9338496a3af4857befc040
Jenn <znzhenn@gmail.com>
2025-10-22 22:34:51 -0400

    Checkpoint 3 (pt3) - update 3
    * updating factorial api

--- test/smokeTests/FactorialAPITest.java
+++ test/smokeTests/FactorialAPITest.java
@@ -3,8 +3,10 @@ package smokeTests;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import projectapis.conceptual.FactorialAPI;
+import projectapis.conceptual.FactorialAPIImplementation;
 
 public class FactorialAPITest {
 
@@ -12,9 +14,24 @@ public class FactorialAPITest {
     void smokeTestFactorialAPI() {
         FactorialAPI api = Mockito.mock(FactorialAPI.class);
         Mockito.when(api.computeFactorial(5)).thenReturn(120);
-
         long result = api.computeFactorial(5);
 
-        assertEquals(25, result);
+        assertEquals(120, result);
+        
+        new projectapis.conceptual.FactorialAPIImplementation();
+        assertNotNull(api);
+        
+        
+    
+        
+        /*
+      //doing the actual computation
+    	long factorialOfSum();
+
+    	//necessary for the implementation
+    	int computeFactorial(int n);
+    	long computeDigitFactorialSum(int number); */
+        
+        
     }
 }
