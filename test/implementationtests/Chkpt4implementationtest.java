aa8d3339a2b4479aaa103ec74888b03c7949f241
Jenn <znzhenn@gmail.com>
2025-11-29 19:56:37 -0500

    updating import statement

--- test/implementationtests/Chkpt4implementationtest.java
+++ test/implementationtests/Chkpt4implementationtest.java
@@ -1,6 +1,6 @@
 package implementationtests;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import projectapis.conceptual.FactorialAPI;
