3f635205a4dcd5e258ae4e99d051324ab6f71eb6
Jenn <znzhenn@gmail.com>
2025-12-15 16:37:49 -0500

    results recorded and optimized : )

--- src/test/FactorialBottleneckBenchmarkTest.java
+++ src/test/FactorialBottleneckBenchmarkTest.java
@@ -1,17 +1,21 @@
 package test;
 import projectapis.conceptual.FactorialAPI;
+import projectapis.conceptual.OptimizedFactorialAPIImplementation;
+
 import projectapis.conceptual.FactorialAPIImplementation;
 
 public class FactorialBottleneckBenchmarkTest {
 
     public static void main(String[] args) {
-        FactorialAPI api = new FactorialAPIImplementation();
+        //FactorialAPI api = new FactorialAPIImplementation();
+        FactorialAPI api = new OptimizedFactorialAPIImplementation();
+
 
         // heavy numbers
         int[] testNumbers = {725102312, 91734610, 1238141088, 223414021};
 
      	// jvm warm-up
-        for (int i = 0; i < 10_000; i++) {
+        for (int i = 0; i < 100000; i++) {
             for (int number : testNumbers) {
                 api.computeDigitFactorialSum(number);
             }
@@ -20,7 +24,7 @@ public class FactorialBottleneckBenchmarkTest {
         long start = System.nanoTime();
         long last = 0L;
 
-        int iterations = 100000;
+        int iterations = 1000000;
         for (int i = 0; i < iterations; i++) {
         	for (int number : testNumbers) {
         		last = api.computeDigitFactorialSum(number);     	
@@ -32,8 +36,13 @@ public class FactorialBottleneckBenchmarkTest {
         System.out.println("Execution time (ms): " + ((end - start) / 1_000_000));
     }
     
-    /* results
+    /* non-optimized
      * last result: 63
-     * execution time (ms)11
+     * execution time (ms) 112
+     * */
+    
+    /* optimized
+     * last result:
+     * execution time (ms) 59
      * */
 }
