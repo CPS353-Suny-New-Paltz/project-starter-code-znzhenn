674255d727b12fcfb3be6f71259392d607adcf93
Jenn <znzhenn@gmail.com>
2025-12-15 16:21:07 -0500

    bottleneck test for factorial computation using large numberes

---
+++ src/test/BottleneckTest.java
@@ -0,0 +1,24 @@
+package test;
+import projectapis.conceptual.FactorialAPI;
+import projectapis.conceptual.FactorialAPIImplementation;
+
+public class BottleneckTest {
+
+    public static void main(String[] args) {
+        FactorialAPI api = new FactorialAPIImplementation();
+
+        // heavy numbers
+        int[] testNumbers = {725102312, 91734610, 1238141088, 223414021};
+
+        long start = System.nanoTime();
+
+        long last = 0L;
+        for (int number : testNumbers) {
+            last = api.computeDigitFactorialSum(number);
+        }
+
+        long end = System.nanoTime();
+        System.out.println("Last result: " + last);
+        System.out.println("Execution time (ms): " + ((end - start) / 1_000_000));
+    }
+}
